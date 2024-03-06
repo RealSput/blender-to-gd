beb15398aed2931464bd15da77f2200f59f2b800
your friendly trustworthy programmer <117580169+RealSput@users.noreply.github.com>
2024-03-06 00:51:18 -0600

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -33,6 +33,39 @@ const stripUnneededStatements = (mtlString, statementsToOmit) => {
 
     return strippedString.split(/\r?\n/).filter(line => line.trim() !== '').join('\n');
 };
+const rgb2hsv = (r, g, b) => {
+    let rabs, gabs, babs, rr, gg, bb, h, s, v, diff, diffc, percentRoundFn;
+    rabs = r / 255;
+    gabs = g / 255;
+    babs = b / 255;
+    v = Math.max(rabs, gabs, babs),
+    diff = v - Math.min(rabs, gabs, babs);
+    diffc = c => (v - c) / 6 / diff + 1 / 2;
+    percentRoundFn = num => Math.round(num * 100) / 100;
+    if (diff == 0) {
+        h = s = 0;
+    } else {
+        s = diff / v;
+        rr = diffc(rabs);
+        gg = diffc(gabs);
+        bb = diffc(babs);
+
+        if (rabs === v) {
+            h = bb - gg;
+        } else if (gabs === v) {
+            h = (1 / 3) + rr - bb;
+        } else if (babs === v) {
+            h = (2 / 3) + gg - rr;
+        }
+        if (h < 0) {
+            h += 1;
+        }else if (h > 1) {
+            h -= 1;
+        }
+    }
+    let [hue, saturation, brightness] = [Math.round(h * 360), percentRoundFn(s * 100) / 100, percentRoundFn(v * 100) / 100]
+	return `${hue}a${saturation}a${brightness}a0a0`;
+}
 const fpsToSeconds = fps => (1 / fps).toFixed(4);
 
 let black_color = unknown_c();
@@ -42,10 +75,21 @@ let default_color = unknown_c();
 let uc = unknown_c();
 let invis_color = unknown_c();
 invis_color.set(rgba(0, 0, 0, 0), 0, true);
+let red_color = unknown_c();
+red_color.set([255, 0, 0]);
 
 let colors_u, frame_delay;
 let cull_op = false;
 let intensity_op = 1;
+let vert_colors = null;
+let vc_groups = {};
+
+let calculateSign = (face) => {
+    const ab = [face[1][0] - face[0][0], face[1][1] - face[0][1]];
+    const ac = [face[2][0] - face[0][0], face[2][1] - face[0][1]]; 
+
+    return ab[0] * ac[1] - ac[0] * ab[1];
+}
 
 let obj_to_grad = (material, str, offset_x = 0, offset_y = 0, add = true, old_pos, fid, light_pos, scale) => {
     material = stripUnneededStatements(material, ['Ns', 'Ke', 'Ni'])
@@ -88,13 +132,12 @@ let obj_to_grad = (material, str, offset_x = 0, offset_y = 0, add = true, old_po
         let real_diff = [x - old_pos[curr_vert][1], y - old_pos[curr_vert][2]]
         let diff = [real_diff[0] + offset_x, real_diff[1] + offset_y]
         ggroups[curr_vert] = [old_pos[curr_vert][0], x, y];
-		if (real_diff[0] || real_diff[1]) old_pos[curr_vert][0].move(...diff, frame_delay, NONE, 2, 1, 1, false, false);
+		old_pos[curr_vert][0].move(...diff, frame_delay, NONE, 2, 1, 1, false, false);
         curr_vert++;
         return old_pos[curr_vert - 1][0];
     };
-    const quad = (bl, br, tl, tr, col = 1, bgr = 1, layer, col2 = col, addBlend = false) => {
-        // little cheat cuz i didn't want to change G.js
-        const hsv = `0a1a${bgr}a0a0,174,${addBlend ? 1 : 0}`;
+    const quad = (bl, br, tl, tr, col = 1, bgr = 1, layer, col2 = col, addBlend = false, customHSV = false) => {
+        const hsv = customHSV ? customHSV : `0a1a${bgr}a0a0`;
         const o = {
             OBJ_ID: 2903,
             Y: 100 + (grad_id + 10000) * 10 * 3,
@@ -107,6 +150,7 @@ let obj_to_grad = (material, str, offset_x = 0, offset_y = 0, add = true, old_po
             GR_ID: grad_id,
             COLOR: col,
             COLOR_2: col2,
+			GR_BLENDING: addBlend ? 1 : 0,
             PREVIEW_OPACITY: 1,
             COLOR_2_HVS_ENABLED: true,
             COLOR_2_HVS: hsv,
@@ -117,7 +161,7 @@ let obj_to_grad = (material, str, offset_x = 0, offset_y = 0, add = true, old_po
         ord++;
         grad_id++;
     };
-    const tri = (v1, v2, v3, col = 1, bgr = 1, layer, col2 = col, addBlend = false) => quad(v1, v2, v3, v3, col, bgr, layer, col2, addBlend); 
+    const tri = (v1, v2, v3, col = 1, bgr = 1, layer, col2 = col, addBlend = false, customHSV = false) => quad(v1, v2, v3, v3, col, bgr, layer, col2, addBlend, customHSV); 
     const vertexToLight = normalize({
         x: light_pos[0],
         y: light_pos[1],
@@ -127,7 +171,7 @@ let obj_to_grad = (material, str, offset_x = 0, offset_y = 0, add = true, old_po
 
     const centerPos = {
         x: 200,
-        y: 390
+        y: 150
     };
     let vertexToGid = {};
 
@@ -150,6 +194,7 @@ let obj_to_grad = (material, str, offset_x = 0, offset_y = 0, add = true, old_po
     for (let f of $obj.faces) {
         const face_color = f.material == '' ? default_color : colors_u[f.material];
         const vs = f.vertices;
+		let face_verts = vs.map(x => [$obj.vertices[x.vertexIndex - 1].x, $obj.vertices[x.vertexIndex - 1].y])
 
         const l1 = verticesLight[vs[0].vertexNormalIndex - 1];
         const l2 = verticesLight[vs[1].vertexNormalIndex - 1];
@@ -160,12 +205,19 @@ let obj_to_grad = (material, str, offset_x = 0, offset_y = 0, add = true, old_po
         const v3 = vertexToGid[vs[2].vertexIndex];
 		
 		const cameraPosition = { x: 0, y: 0, z: -50 };
-		const culled = cull_op ? (($obj.vertices[vs[1].vertexIndex - 1].x - $obj.vertices[vs[0].vertexIndex - 1].x) * ($obj.vertices[vs[2].vertexIndex - 1].y - $obj.vertices[vs[0].vertexIndex - 1].y) - ($obj.vertices[vs[1].vertexIndex - 1].y - $obj.vertices[vs[0].vertexIndex - 1].y) * ($obj.vertices[vs[2].vertexIndex - 1].x - $obj.vertices[vs[0].vertexIndex - 1].x)) < 0 : false;
-
+		const culled = cull_op ? calculateSign(face_verts) < 0 : false;
         let depth = avr($obj.vertices[vs[0].vertexIndex - 1].z, $obj.vertices[vs[1].vertexIndex - 1].z, $obj.vertices[vs[2].vertexIndex - 1].z);
-
-        asf.push({vs: [v3, v1, v2], depth, c1: black_color, c2: face_color, bgr: l1, blending: false, culled}, {vs: [v1, v2, v3], depth, c1: black_color, c2: face_color, bgr: l2, blending: true, culled}, {vs: [v2, v3, v1], depth, c1: black_color, c2: face_color, bgr: l3, blending: true, culled});
-	}
+		let vertex_cols_curr = []; // where all 3 vertex colors of the face will be stored
+		// loops through all 3 vertices
+		let add_gr = [{vs: [v3, v1, v2], depth, c1: black_color, c2: !vert_colors ? face_color : red_color, bgr: !vert_colors ? l1 : 1, blending: false, culled}, {vs: [v1, v2, v3], depth, c1: black_color, c2: !vert_colors ? face_color : red_color, bgr: !vert_colors ? l2 : 1, blending: true, culled}, {vs: [v2, v3, v1], depth, c1: black_color, c2: !vert_colors ? face_color : red_color, bgr: !vert_colors ? l3 : 1, blending: true, culled}];
+		add_gr = add_gr.map((x, i) => {
+			if (vert_colors) {
+				x.hsvc = rgb2hsv(...vert_colors[vs[i].vertexIndex])
+			};
+			return x
+		})
+		asf.push(...add_gr);
+    }
 
     asf = asf.sort((a, b) => a.depth - b.depth);
 
@@ -175,7 +227,7 @@ let obj_to_grad = (material, str, offset_x = 0, offset_y = 0, add = true, old_po
     let layer = lrs;
     for (let a of asf) {
         for (let f of a) {
-            if (!f.culled) tri(f.vs[0], f.vs[1], f.vs[2], f.c1, f.bgr, layer, f.c2, f.blending);
+            if (!f.culled) tri(f.vs[0], f.vs[1], f.vs[2], f.c1, f.bgr, layer, f.c2, f.blending, f.hsvc);
         }
         layer++;
     }
@@ -191,8 +243,8 @@ module.exports = async (config) => {
 	let light_pos = config.light_pos ?? [-0.5, 1, 0.5] // position of light (x, y, z)
 	let file_path = config.file_name; // path of JSON input
 	let pos = config.pos ?? [-40, -60]; // offset of rendered scene, xy coordinates
-	let lock = config.lock ?? true; // whether the scene should be locked to player X or not
-	let loop = config.loop ?? true; // whether animation should loop forever or not
+	let lock = config.lock ?? false; // whether the scene should be locked to player X or not
+	let loop = config.loop ?? false; // whether animation should loop forever or not
 	let fps = config.fps ?? 24; // frames per sec
 	let scale = config.scale ?? 1;
 	cull_op = config.cull_faces ?? cull_op
@@ -221,6 +273,7 @@ module.exports = async (config) => {
 	let file = await readFile(file_path);
 	let objs = JSON.parse(file);
 	let materials = objs[0];
+	if (objs[3] !== 0) vert_colors = objs[3];
 	objs = objs[1]
 
 	let fid = 0;
